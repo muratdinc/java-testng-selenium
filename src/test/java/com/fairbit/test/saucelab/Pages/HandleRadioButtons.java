0ef6cd0fbd190e321222961587a63e0eaa71996a
ozgur.altuntas <ozgur.altuntas@fairbit.com>
2017-11-05 22:24:38 -0800

    Added radio button list control

--- src/test/java/com/fairbit/test/saucelab/Pages/HandleRadioButtons.java
+++ src/test/java/com/fairbit/test/saucelab/Pages/HandleRadioButtons.java
@@ -1,7 +1,45 @@
 package com.fairbit.test.saucelab.Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
 /**
- * Created by ozgur.altuntas on 11/5/2017.
+ * Created by OZGUR-FAIRBIT on 11/3/2017.
  */
 public class HandleRadioButtons {
+
+    public WebDriver driver;
+    public static String url = "http://seleniumpractise.blogspot.com/2016/08/how-to-automate-radio-button-in.html";
+
+    public static HandleRadioButtons visitPage(WebDriver driver) {
+        HandleRadioButtons page = new HandleRadioButtons(driver);
+        page.visitPage();
+        return page;
+    }
+
+    public HandleRadioButtons(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void visitPage() {
+        this.driver.get(url);
+    }
+
+    public String GetRadioButtonValues() {
+
+        List<WebElement> radio=driver.findElements ( By.xpath ( "//input[@name='lang' and @type='radio']" ) );
+
+        String listvalue = "";
+        for (int i=0;i<radio.size ();i++){
+            WebElement local_radio=radio.get(i);
+            String value = local_radio.getAttribute ( "value" );
+            listvalue += value;
+        }
+        return listvalue;
+    }
 }
--- src/test/java/com/fairbit/test/saucelab/Tests/TestHandleRadioButtons.java
+++ src/test/java/com/fairbit/test/saucelab/Tests/TestHandleRadioButtons.java
@@ -1,7 +1,36 @@
 package com.fairbit.test.saucelab.Tests;
 
-/**
- * Created by ozgur.altuntas on 11/5/2017.
- */
-public class TestHandleRadioButtons {
+import com.fairbit.test.saucelab.Pages.BootstrapDropdown;
+import com.fairbit.test.saucelab.Pages.HandleRadioButtons;
+import org.openqa.selenium.InvalidElementStateException;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.rmi.UnexpectedException;
+
+
+public class TestHandleRadioButtons extends TestBase {
+
+    /**
+     * Runs a simple test verifying link can be followed.
+     *
+     * @throws InvalidElementStateException
+     */
+    @Test(dataProvider = "hardCodedBrowsers")
+    public void VerifyHandleRadioButtons(String browser, String version, String os, Method method)
+            throws MalformedURLException, InvalidElementStateException, UnexpectedException {
+
+        //create webdriver session
+        this.createDriver(browser, version, os, method.getName());
+        WebDriver driver = this.getWebDriver();
+
+        this.annotate("Visiting Related pages...");
+        HandleRadioButtons page = HandleRadioButtons.visitPage(driver);
+
+        this.annotate("Dropdown month value ...");
+        Assert.assertTrue(page.GetRadioButtonValues().equals("JAVAC#PYTHONRUBY"));
+    }
 }
