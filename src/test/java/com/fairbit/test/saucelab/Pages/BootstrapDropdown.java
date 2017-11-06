e5b31884ceb0387611b0cffbfb446af8b0885508
ozgur.altuntas <ozgur.altuntas@fairbit.com>
2017-11-05 22:24:03 -0800

    Added bootstrap dropdown list

--- src/test/java/com/fairbit/test/saucelab/Pages/BootstrapDropdown.java
+++ src/test/java/com/fairbit/test/saucelab/Pages/BootstrapDropdown.java
@@ -1,7 +1,45 @@
 package com.fairbit.test.saucelab.Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import java.util.List;
+
 /**
- * Created by ozgur.altuntas on 11/5/2017.
+ * Created by OZGUR-FAIRBIT on 11/3/2017.
  */
 public class BootstrapDropdown {
+
+    public WebDriver driver;
+    public static String url = "http://seleniumpractise.blogspot.com/2016/08/bootstrap-dropdown-example-for-selenium.html";
+
+    public static BootstrapDropdown visitPage(WebDriver driver) {
+        BootstrapDropdown page = new BootstrapDropdown(driver);
+        page.visitPage();
+        return page;
+    }
+
+    public BootstrapDropdown(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void visitPage() {
+        this.driver.get(url);
+    }
+
+    public String SelectBootstrapDDValues() {
+
+        driver.findElement ( By.xpath ( "//*[@id=\"menu1\"]" ) ).click ();
+
+        List<WebElement> dd_menu=driver.findElements ( By.xpath ( "//ul[@class='dropdown-menu']//li/a" ) );
+        String listhtml = "";
+        for (int i=0;i<dd_menu.size ();i++){
+            WebElement element=dd_menu.get ( i );
+            String innerhtml=element.getAttribute ( "innerHTML" );
+            listhtml += innerhtml;
+        }
+        return listhtml;
+    }
 }
--- src/test/java/com/fairbit/test/saucelab/Tests/TestBootstrapDropdown.java
+++ src/test/java/com/fairbit/test/saucelab/Tests/TestBootstrapDropdown.java
@@ -1,7 +1,34 @@
 package com.fairbit.test.saucelab.Tests;
 
-/**
- * Created by ozgur.altuntas on 11/5/2017.
- */
-public class TestBootstrapDropdown {
+import com.fairbit.test.saucelab.Pages.BootstrapDropdown;
+import org.openqa.selenium.InvalidElementStateException;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.rmi.UnexpectedException;
+
+public class TestBootstrapDropdown extends TestBase {
+
+    /**
+     * Runs a simple test verifying link can be followed.
+     *
+     * @throws InvalidElementStateException
+     */
+    @Test(dataProvider = "hardCodedBrowsers")
+    public void VerifyBootstrapDropdownSelection(String browser, String version, String os, Method method)
+            throws MalformedURLException, InvalidElementStateException, UnexpectedException {
+
+        //create webdriver session
+        this.createDriver(browser, version, os, method.getName());
+        WebDriver driver = this.getWebDriver();
+
+        this.annotate("Visiting Facebook login page...");
+        BootstrapDropdown page = BootstrapDropdown.visitPage(driver);
+
+        this.annotate("Dropdown month value ...");
+        Assert.assertTrue(page.SelectBootstrapDDValues().equals("HTMLCSSJavaScriptAbout Us"));
+    }
 }
