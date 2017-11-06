c5b46e8f46f4fdac45c438a702e967641f6dc922
ozgur.altuntas <ozgur.altuntas@fairbit.com>
2017-11-05 21:04:50 -0800

    Added sample source Bootstrap Dropdown to the selenium

---
+++ src/test/java/com/fairbit/test/saucelab/Pages/BootstrapDropdown.java
@@ -0,0 +1,7 @@
+package com.fairbit.test.saucelab.Pages;
+
+/**
+ * Created by ozgur.altuntas on 11/5/2017.
+ */
+public class BootstrapDropdown {
+}
---
+++ src/test/java/com/fairbit/test/saucelab/Tests/TestBootstrapDropdown.java
@@ -0,0 +1,7 @@
+package com.fairbit.test.saucelab.Tests;
+
+/**
+ * Created by ozgur.altuntas on 11/5/2017.
+ */
+public class TestBootstrapDropdown {
+}
